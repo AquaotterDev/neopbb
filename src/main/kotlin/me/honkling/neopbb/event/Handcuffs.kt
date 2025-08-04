d9d8dc3cc013ad66b31e109cdb16e1cba3c66d53
honkling <untitledgeese@gmail.com>
2025-08-04 02:00:50 -0500

    feat: keycards

---
+++ src/main/kotlin/me/honkling/neopbb/event/Handcuffs.kt
@@ -0,0 +1,4 @@
+
+
+package me.honkling.neopbb.event
+
--- src/main/kotlin/me/honkling/neopbb/event/Keycards.kt
+++ src/main/kotlin/me/honkling/neopbb/event/Keycards.kt
@@ -1,2 +1,33 @@
+@file:Listener
+
 package me.honkling.neopbb.event
 
+import me.honkling.commando.spigot.event.Listener
+import me.honkling.neopbb.profile.keycard
+import me.honkling.neopbb.world
+import net.kyori.adventure.key.Key
+import net.kyori.adventure.sound.Sound
+import org.bukkit.block.data.type.Door
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
+
+private fun onInteract(event: PlayerInteractEvent) {
+    val player = event.player
+    val hand = event.hand ?: return
+    val block = event.clickedBlock ?: return
+    val state = block.state
+    val data = state.blockData as? Door ?: return
+
+    val mainItem = player.inventory.getItem(hand).asOne()
+    val offItem = player.inventory.getItem(hand.oppositeHand).asOne()
+    if ("TRAPDOOR" in block.type.name || mainItem != keycard || (offItem == keycard && hand != EquipmentSlot.HAND))
+        return
+
+    block.world.playSound(Sound.sound {
+        it.type(Key.key("minecraft:block.iron_door.open"))
+    }, block.x.toDouble(), block.y.toDouble(), block.z.toDouble())
+    event.isCancelled = true
+    data.isOpen = !data.isOpen
+    state.blockData = data
+    state.update()
+}
