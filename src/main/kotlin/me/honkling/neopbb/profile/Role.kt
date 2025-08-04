06315494e0b40352993591f0ad658e8f4652335f
honkling <untitledgeese@gmail.com>
2025-08-03 19:13:42 -0500

    fix: compile error

--- src/main/kotlin/me/honkling/neopbb/profile/Role.kt
+++ src/main/kotlin/me/honkling/neopbb/profile/Role.kt
@@ -2,7 +2,7 @@ package me.honkling.neopbb.profile
 
 import me.honkling.neopbb.instance
 import me.honkling.neopbb.lib.builder
-import me.honkling.neopbb.lib.goldenApple
+import me.honkling.neopbb.lib.illegalGoldenApple
 import me.honkling.neopbb.lib.mm
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.sound.Sound
@@ -242,7 +242,7 @@ enum class Role(
         inventory.setItem(EquipmentSlot.LEGS, leggings)
         inventory.setItem(EquipmentSlot.FEET, boots)
         inventory.addItem(sword)
-        inventory.addItem(goldenApple.asQuantity(4))
+        inventory.addItem(illegalGoldenApple.asQuantity(4))
     }),
     Prisoner(false, {
         val chestplate = ItemStack(Material.LEATHER_CHESTPLATE)
