8bc1e8edba4bdc2ffc9c855ed710890a44181533
honkling <untitledgeese@gmail.com>
2025-08-06 23:03:32 -0500

    fix: floor warden timer

--- src/main/kotlin/me/honkling/neopbb/task/ActionBar.kt
+++ src/main/kotlin/me/honkling/neopbb/task/ActionBar.kt
@@ -13,7 +13,7 @@ internal fun executeActionBar() {
 
     val wardenStatus = if (warden != null) "Current warden: <s>${warden!!.name}"
         else if (wardenCooldown <= 0) "Say <s>/warden</s> to become warden"
-        else "Please wait <s>${wardenCooldown / 20.0} seconds</s>"
+        else "Please wait <s>${wardenCooldown / 20} seconds</s>"
 
     for (player in Bukkit.getOnlinePlayers())
         player.sendActionBar("$wardenStatus <gray>|</gray> <s>${formatCurrency(player.money)}".mm)
