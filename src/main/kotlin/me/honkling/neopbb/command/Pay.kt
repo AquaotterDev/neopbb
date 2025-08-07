7b8b7626e1e0555ac694e3f18f04dcf783a2cba6
honkling <untitledgeese@gmail.com>
2025-08-07 13:58:44 -0500

    feat: /pay, /balance

---
+++ src/main/kotlin/me/honkling/neopbb/command/Pay.kt
@@ -0,0 +1,25 @@
+@file:Command("pay")
+
+package me.honkling.neopbb.command
+
+import me.honkling.commando.spigot.command.Command
+import me.honkling.neopbb.lib.formatCurrency
+import me.honkling.neopbb.lib.mm
+import me.honkling.neopbb.profile.money
+import org.bukkit.entity.Player
+
+private fun pay(sender: Player, target: Player, amount: Float) {
+    if (sender == target)
+        return sender.sendMessage("<p>You can't pay yourself.".mm)
+
+    if (amount < 100.0)
+        return sender.sendMessage("<p>You must pay <s>$100</s> or more.".mm)
+
+    if (sender.money < amount)
+        return sender.sendMessage("<p>You don't have that much money.".mm)
+
+    sender.money -= amount
+    target.money += amount
+    sender.sendMessage("<p>You paid <s>${formatCurrency(amount)}</s> to <s>${target.name}</s>.".mm)
+    target.sendMessage("<p>You received <s>${formatCurrency(amount)}</s> from <s>${sender   .name}</s>.".mm)
+}
