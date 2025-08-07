c65af61f2923c7e13a582cdf882c116226611a13
honkling <untitledgeese@gmail.com>
2025-08-06 23:45:55 -0500

    fix: adjust warden cooldown display

--- src/main/kotlin/me/honkling/neopbb/task/ActionBar.kt
+++ src/main/kotlin/me/honkling/neopbb/task/ActionBar.kt
@@ -6,6 +6,7 @@ import me.honkling.neopbb.profile.money
 import me.honkling.neopbb.profile.warden
 import me.honkling.neopbb.profile.wardenCooldown
 import org.bukkit.Bukkit
+import kotlin.math.ceil
 
 internal fun executeActionBar() {
     if (wardenCooldown > 0)
@@ -13,7 +14,11 @@ internal fun executeActionBar() {
 
     val wardenStatus = if (warden != null) "Current warden: <s>${warden!!.name}"
         else if (wardenCooldown <= 0) "Say <s>/warden</s> to become warden"
-        else "Please wait <s>${wardenCooldown / 20} seconds</s>"
+        else {
+            val cooldown = ceil(wardenCooldown / 20.0).toInt()
+            val unit = if (cooldown == 1) "second" else "seconds"
+            "Please wait <s>$cooldown $unit</s>"
+        }
 
     for (player in Bukkit.getOnlinePlayers())
         player.sendActionBar("$wardenStatus <gray>|</gray> <s>${formatCurrency(player.money)}".mm)
